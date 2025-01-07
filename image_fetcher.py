1291ed15c5796fe41fa2896b4f214f90c33058b2
RMSCA <howkurumi1@gmail.com>
2025-01-07 22:30:45 +0800

    bug fix

--- image_fetcher.py
+++ image_fetcher.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/get-image', methods=['POST'])
 def get_image():
